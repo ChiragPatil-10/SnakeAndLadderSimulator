8a08aa330951f7c785b01f01d5a2a5df3d2c449a
Chirag Patil <patilchirag192k@gmail.com>
2024-11-27 17:04:43 +0530

    Added : UC3 Checking the option

--- src/SnakeLadderProblem.java
+++ src/SnakeLadderProblem.java
@@ -10,6 +10,35 @@ public class SnakeLadderProblem {
         Random random = new Random();
         int dieRoll = random.nextInt(6) + 1;
         System.out.println("The Player rolled: " + dieRoll);
+
+        int option = random.nextInt(3);
+
+        switch (option) {
+            case 0:
+                System.out.println("Option: No Play. Player stays at postion: " + start_position);
+                break;
+
+            case 1:
+                start_position += dieRoll;
+                System.out.println("Option: Ladder!!. Player climbs up to position: " + start_position);
+                break;
+
+            case 2:
+                start_position -= dieRoll;
+                if (start_position < 0) {
+                    start_position = 0;
+                }
+                System.out.println("Option: Snake! Player slides down to position: " + start_position);
+                break;
+
+            default:
+                System.out.println("Invalid option!");
+        }
     }
 }
+
+
+
+
+
 
