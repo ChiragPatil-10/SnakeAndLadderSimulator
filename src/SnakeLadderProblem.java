5d3a5955fcb1baf8109d9609bb39ad9882df81e5
Chirag Patil <patilchirag192k@gmail.com>
2024-11-27 17:49:20 +0530

    Added : UC4 Winning Position

--- src/SnakeLadderProblem.java
+++ src/SnakeLadderProblem.java
@@ -5,36 +5,40 @@ public class SnakeLadderProblem {
         System.out.println("Welcome to the Snake Ladder Simulator");
 
         int start_position = 0;
+        int winningPosition = 100;
         System.out.println("Player Start at position: " + start_position);
 
         Random random = new Random();
-        int dieRoll = random.nextInt(6) + 1;
-        System.out.println("The Player rolled: " + dieRoll);
+        while (start_position < winningPosition) {
+            int dieRoll = random.nextInt(6) + 1;
+            System.out.println("The Player rolled: " + dieRoll);
 
-        int option = random.nextInt(3);
+            int option = random.nextInt(3);
 
-        switch (option) {
-            case 0:
-                System.out.println("Option: No Play. Player stays at postion: " + start_position);
-                break;
+            switch (option) {
+                case 0:
+                    System.out.println("Option: No Play. Player stays at postion: " + start_position);
+                    break;
 
-            case 1:
-                start_position += dieRoll;
-                System.out.println("Option: Ladder!!. Player climbs up to position: " + start_position);
-                break;
+                case 1:
+                    start_position += dieRoll;
+                    System.out.println("Option: Ladder!!. Player climbs up to position: " + start_position);
+                    break;
 
-            case 2:
-                start_position -= dieRoll;
-                if (start_position < 0) {
-                    start_position = 0;
-                }
-                System.out.println("Option: Snake! Player slides down to position: " + start_position);
-                break;
-
-            default:
-                System.out.println("Invalid option!");
+                case 2:
+                    start_position -= dieRoll;
+                    if (start_position < 0) {
+                        start_position = 0;
+                    }
+                    System.out.println("Option: Snake! Player slides down to position: " + start_position);
+                    break;
         }
+
+            System.out.println("Current Position: " + start_position);
+        }
+        System.out.println("Congratulations!! The player has reached the winning position. " + winningPosition);
     }
+
 }
 
 
