e41161c9b6c8f317fbc6f399924059460fb1cfce
Chirag Patil <patilchirag192k@gmail.com>
2024-11-27 15:05:41 +0530

    Added : Rolls the Die

--- src/SnakeLadderProblem.java
+++ src/SnakeLadderProblem.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 public class SnakeLadderProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake Ladder Simulator");
 
         int start_position = 0;
         System.out.println("Player Start at position: " + start_position);
+
+        Random random = new Random();
+        int dieRoll = random.nextInt(6) + 1;
+        System.out.println("The Player rolled: " + dieRoll);
     }
 }
 
