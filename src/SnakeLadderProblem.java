4cb4dd5f5a2c254d0228e17da96856b3e43a91f3
Chirag Patil <patilchirag192k@gmail.com>
2024-11-27 14:45:11 +0530

    Added : PLayer Start Position

--- src/SnakeLadderProblem.java
+++ src/SnakeLadderProblem.java
@@ -1,7 +1,9 @@
 public class SnakeLadderProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake Ladder Simulator");
-    }
 
+        int start_position = 0;
+        System.out.println("Player Start at position: " + start_position);
+    }
 }
 
